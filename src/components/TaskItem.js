3769b7329e3b0e14b3f667815f4f194aae2498c9
Londort <landort@gmail.com>
2024-01-28 17:03:36 +0100

    added icon defor title in TaskItem

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,9 +1,11 @@
 import styles from './TaskItem.module.css';
+import { FaRegEdit } from 'react-icons/fa';
 
 const TaskItem = ({ task, deleteTask, onTaskClick }) => {
   return (
     <article className={styles.container} onClick={() => onTaskClick(task)}>
       <div className={styles.header}>
+        <FaRegEdit className={styles.icon} />
         <p>{task.title}</p>
       </div>
       <div className={styles.footer}>
